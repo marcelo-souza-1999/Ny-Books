7232ec85cb9837264e1fa252a6ebb53df20ed57d
Marcelo-Dev-Android <developermarcelosouza@gmail.com>
2021-10-17 00:10:44 -0300

    Retrofit configuration and Network Layer completed

--- app/src/main/java/com/marcelo/nybooks/network/service/NYTBooksService.kt
+++ app/src/main/java/com/marcelo/nybooks/network/service/NYTBooksService.kt
@@ -1,11 +1,16 @@
 package com.marcelo.nybooks.network.service
 
+import com.marcelo.nybooks.BuildConfig
+import com.marcelo.nybooks.network.response.BooksBodyResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NYTBooksService {
 
     @GET("lists.json")
-    fun listRepos(): Call<List<?>>
-
+    fun getBooks(
+        @Query("api-key") apiKey: String = BuildConfig.KEY_BOOKS_API,
+        @Query("list") list: String = "hardcover-fiction"
+    ): Call<BooksBodyResponse?>
 }
