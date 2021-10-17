3f79e3b28da2cb1b8c3a5d397216df7eede48754
Marcelo-Dev-Android <developermarcelosouza@gmail.com>
2021-10-17 14:09:32 -0300

    Exhibition of books completed

--- app/src/main/java/com/marcelo/nybooks/ui/viewmodel/BooksViewModel.kt
+++ app/src/main/java/com/marcelo/nybooks/ui/viewmodel/BooksViewModel.kt
@@ -3,20 +3,42 @@ package com.marcelo.nybooks.ui.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.marcelo.nybooks.network.model.Books
+import com.marcelo.nybooks.network.response.BooksBodyResponse
+import com.marcelo.nybooks.network.service.ApiService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class BooksViewModel : ViewModel() {
 
     val booksLiveData: MutableLiveData<List<Books>> = MutableLiveData()
 
     fun getBooks() {
-        booksLiveData.value = createFakeBooks()
-    }
+       ApiService.service.getBooks().enqueue(object: Callback<BooksBodyResponse?>
+       {
+           override fun onResponse(call: Call<BooksBodyResponse?>, response: Response<BooksBodyResponse?>)
+           {
+                if (response.isSuccessful){
+                    val books: MutableList<Books> = mutableListOf()
+                    response.body()?.let { booksBodyResponse ->
+                        for (result in booksBodyResponse.booksResults)
+                        {
+                            val book = Books(
+                                title = result.bookDetails[0].title,
+                                author = result.bookDetails[0].author
+                            )
+                            books.add(book)
+                        }
+                    }
+                    booksLiveData.value = books
+                }
+           }
 
-    private fun createFakeBooks(): List<Books> {
-        return listOf(
-            Books("Titulo Um", "Autor um"),
-            Books("Titulo Dois", "Autor dois"),
-            Books("Titulo Tres", "Autor tres"),
-        )
+           override fun onFailure(call: Call<BooksBodyResponse?>, t: Throwable) {
+
+           }
+       })
     }
 }
+
+
